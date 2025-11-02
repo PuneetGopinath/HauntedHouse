420bb7304825e3a466644b761ceca84130d9912b
Puneet Gopinath <76863199+PuneetGopinath@users.noreply.github.com>
2025-11-02 19:37:12 +0530

    feat: add initial HTML and JSX files for Haunted House project

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+/**
+ * HauntedHouse
+ * © 2025 Puneet Gopinath. All rights reserved.
+ * Filename: src/index.jsx
+ * License: MIT (see LICENSE)
+ */
+
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
